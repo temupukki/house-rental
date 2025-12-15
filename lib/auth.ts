ce6fbd3b1d3f1538a2e6d347101a909c6200bf6b
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-12-15 19:52:51 +0300

    betterauth-setup

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "postegresql", 
+        provider: "postgresql", 
     }),
 });
