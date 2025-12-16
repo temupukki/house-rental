71e9db314023c9cb8507b43625bd52c3256c10ca
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-12-16 21:04:17 +0300

    update

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,11 +4,17 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", 
-    }),
-      emailAndPassword: {    
-        enabled: true,
-        autoSignIn:false
-    } 
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
 });
