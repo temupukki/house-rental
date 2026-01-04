a633ad43475bf7716d5133045af21b1ce4f79486
Temesgen Gashaw <temesgengashaw8@gmail.com>
2026-01-04 20:05:32 +0300

    update

--- app/dashboard/(valprop)/listings/page.tsx
+++ app/dashboard/(valprop)/listings/page.tsx
@@ -362,13 +362,7 @@ const filteredProperties = properties
                                 <Edit size={18} />
                               </button>
 
-                              <button
-                               
-                                className="p-2 rounded-lg bg-red-100 text-red-600 hover:bg-red-200"
-                                title="Delete property"
-                              >
-                                <Trash2 size={18} />
-                              </button>
+                          
                             </div>
                           </div>
                         </div>
